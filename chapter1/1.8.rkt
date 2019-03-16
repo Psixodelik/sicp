9e4cf890c6836bbfd5d14382fffac73e7ff40d09
Nikita Mikhaylov <nmikhaylov92@gmail.com>
2019-03-17 01:49:52 +0300

    Add 1.8

---
+++ chapter1/1.8.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+(define (square x)
+  (* x x))
+
+(define (cube x)
+  (* (square x) x))
+
+(define (cbrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cbrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess))
+     3))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) (* guess 0.00000000001)))
+
+(define (cbrt x)
+  (cbrt-iter 1.0 x))
+
+(cbrt 8)
